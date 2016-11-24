2d36c53d186155475dec2803d216399df8394480
DrabWeb <sethunity@gmail.com>
2016-11-24 00:26:54 -0400

    Added darkening the music player window, will be used for displaying the playlist/search

--- Azusa/Azusa/AZMusicPlayerViewController.swift
+++ Azusa/Azusa/AZMusicPlayerViewController.swift
@@ -23,8 +23,26 @@ class AZMusicPlayerViewController: NSViewController {
     
     /// When the user presses 'titlebarPlaylistButton'...
     @IBAction func titlebarPlaylistButtonPressed(_ sender: NSButton) {
-        /// REMOVEME: Small display(coverImage:) test
-        self.display(coverImage: #imageLiteral(resourceName: "AZExampleCoverHauru"));
+        // If we are currently darkening...
+        if(darkening) {
+            // If the darkening is not from the playlist button...
+            if(titlebarContainer.subviews.last != titlebarPlaylistButton) {
+                // Darken and open the playlist
+                darken(withTitlebarItemAbove: titlebarPlaylistButton);
+                openPlaylist();
+            }
+            // If the darkening is from the playlist button...
+            else {
+                // Undarken
+                undarken();
+            }
+        }
+        // If we aren't currently darkening...
+        else {
+            // Darken and open the playlist
+            darken(withTitlebarItemAbove: titlebarPlaylistButton);
+            openPlaylist();
+        }
     }
     
     /// The button in the titlebar for opening the search view
@@ -32,8 +50,26 @@ class AZMusicPlayerViewController: NSViewController {
     
     /// When the user presses 'titlebarSearchButton'...
     @IBAction func titlebarSearchButtonPressed(_ sender: NSButton) {
-        /// REMOVEME: Small display(coverImage:) test
-        self.display(coverImage: #imageLiteral(resourceName: "AZExampleCoverMoe"));
+        // If we are currently darkening...
+        if(darkening) {
+            // If the darkening is not from the search button...
+            if(titlebarContainer.subviews.last != titlebarSearchButton) {
+                // Darken and open search
+                darken(withTitlebarItemAbove: titlebarSearchButton);
+                openSearch();
+            }
+            // If the darkening is from the search button...
+            else {
+                // Undarken
+                undarken();
+            }
+        }
+        // If we aren't currently darkening...
+        else {
+            // Darken and open search
+            darken(withTitlebarItemAbove: titlebarSearchButton);
+            openSearch();
+        }
     }
     
     /// The image view in 'topContainer' that shows the cover image blurred
@@ -45,12 +81,179 @@ class AZMusicPlayerViewController: NSViewController {
     /// The image view for displaying the cover image, child of 'coverImageContainer'
     @IBOutlet weak var coverImageImageView: AZAspectFillImageView!
     
+    /// The label for showing the artist of the current song
+    @IBOutlet weak var currentSongArtistLabel: NSTextField!
+    
+    /// The label for showing the title of the current song
+    @IBOutlet weak var currentSongTitleLabel: NSTextField!
+    
+    /// The container for progress views
+    @IBOutlet weak var progressContainer: NSView!
+    
+    /// The label that shows the current position of the current song
+    @IBOutlet weak var progressCurrentTimeLabel: NSTextField!
+    
+    /// The label that shows the length of the current song
+    @IBOutlet weak var progressSongLengthLabel: NSTextField!
+    
+    /// The slider for showing the current position in the song and allowing the user to scrub
+    @IBOutlet weak var progressSlider: NSSlider!
+    
+    @IBAction func progressSliderMoved(_ sender: NSSlider) {
+        
+    }
+    
+    /// The container view for the playback controls
+    @IBOutlet weak var playbackControlsContainer: NSView!
+    
+    /// The skip previous playback control button
+    @IBOutlet weak var playbackControlsSkipPreviousButton: NSButton!
+    
+    @IBAction func playbackControlsSkipPreviousButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The play/pause playback control button
+    @IBOutlet weak var playbackControlsPausePlayButton: NSButton!
+    
+    @IBAction func playbackControlsPausePlayButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The skip next playback control button
+    @IBOutlet weak var playbackControlsSkipNextButton: NSButton!
+    
+    @IBAction func playbackControlsSkipNextButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The toggle random mode playback control button
+    @IBOutlet weak var playbackControlsRandomButton: NSButton!
+    
+    @IBAction func playbackControlsRandomButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The loop mode playback control button
+    @IBOutlet weak var playbackControlsLoopButton: NSButton!
+    
+    @IBAction func playbackControlsLoopButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The favourite song playback control button
+    @IBOutlet weak var playbackControlsFavouriteButton: NSButton!
+    
+    @IBAction func playbackControlsFavouriteButtonPressed(_ sender: NSButton) {
+        
+    }
+    
+    /// The view that darkens the content of the window when using darkening
+    @IBOutlet weak var contentDarken: AZColoredView!
+    
+    /// The view that darkens the titlebar when using darkening
+    @IBOutlet weak var titlebarDarken: AZColoredView!
+    
+    /// Are we currently darkening the window?
+    var darkening : Bool = false;
+    
+    /// The amount to darken when doing darkening(0 to 1)
+    let darkenAmount : CGFloat = 0.5;
+    
+    /// The length of the darken fade animation
+    let darkenAnimationDuration : CGFloat = 0.25;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         // Style the window
         styleWindow();
+    }
+    
+    /// Opens the playlist view
+    func openPlaylist() {
+        print("Playlist");
+    }
+    
+    /// Opens the search view
+    func openSearch() {
+        print("Search");
+    }
+    
+    /// Darkens the window, and if withTitlebarItemAbove is passed either 'titlebarPlaylistButton' or 'titlebarSearchButton' it will keep that view above the darken
+    func darken(withTitlebarItemAbove: NSView?) {
+        // Print that we are darkening
+//        print("AZMusicPlayerViewController: Darkening view with titlebar item \"\(withTitlebarItemAbove)\" above");
+        
+        // If 'withTitlebarItemAbove' isn't nil...
+        if(withTitlebarItemAbove != nil) {
+            /// Readjust the titlebar subviews so that darken is moved above all, than the given item above darken
+            titlebarContainer.addSubview(titlebarDarken, positioned: .above, relativeTo: nil);
+            titlebarContainer.addSubview(withTitlebarItemAbove!, positioned: .above, relativeTo: titlebarDarken);
+        }
+        // If 'withTitlebarItemAbove' is nil...
+        else {
+            // Move darken above all other subviews
+            titlebarContainer.addSubview(titlebarDarken, positioned: .above, relativeTo: nil);
+        }
+        
+        // If we aren't already darkening...
+        if(!darkening) {
+            /// The animation for fading in the darken views
+            let darkenFade : CABasicAnimation = CABasicAnimation(keyPath: "opacity");
+            
+            // Set the animation duration
+            darkenFade.duration = CFTimeInterval(darkenAnimationDuration);
+            
+            // Set the from value to 0
+            darkenFade.fromValue = 0;
+            
+            // Set the to value to darkenAmount
+            darkenFade.toValue = darkenAmount;
+            
+            // Add the animation to the darken views
+            self.contentDarken.layer?.add(darkenFade, forKey: "animateContents");
+            self.titlebarDarken.layer?.add(darkenFade, forKey: "animateContents");
+            
+            // Set the alpha values so they stay post-animation
+            self.contentDarken.layer?.opacity = Float(darkenAmount);
+            self.titlebarDarken.layer?.opacity = Float(darkenAmount);
+        }
+        
+        // Say we are darkening the window
+        self.darkening = true;
+    }
+    
+    /// Stops darkening this window
+    func undarken() {
+        // If we are currently darkening...
+        if(darkening) {
+            // Print that we are undarkening
+//            print("AZMusicPlayerViewController: Undarkening");
+            
+            /// The animation for fading out the darken views
+            let undarkenFade : CABasicAnimation = CABasicAnimation(keyPath: "opacity");
+            
+            // Set the animation duration
+            undarkenFade.duration = CFTimeInterval(darkenAnimationDuration);
+            
+            // Set the from value to darkenAmount
+            undarkenFade.fromValue = darkenAmount;
+            
+            // Set the to value to 0
+            undarkenFade.toValue = 0;
+            
+            // Add the animation to the darken views
+            self.contentDarken.layer?.add(undarkenFade, forKey: "animateContents");
+            self.titlebarDarken.layer?.add(undarkenFade, forKey: "animateContents");
+            
+            // Set the alpha values so they stay post-animation
+            self.contentDarken.layer?.opacity = 0;
+            self.titlebarDarken.layer?.opacity = 0;
+            
+            // Say we aren't darkening
+            self.darkening = false;
+        }
     }
     
     /// Displays the given NSImage as the cover image for this music player
