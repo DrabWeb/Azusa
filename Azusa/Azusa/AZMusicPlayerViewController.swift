be5bd0d8f2aab2574690ced6680c0751fadd4fed
DrabWeb <sethunity@gmail.com>
2016-11-24 13:22:54 -0400

    Working on the playlist/search views, right now you can open/close them(and they animate)

--- Azusa/Azusa/AZMusicPlayerViewController.swift
+++ Azusa/Azusa/AZMusicPlayerViewController.swift
@@ -25,16 +25,17 @@ class AZMusicPlayerViewController: NSViewController {
     @IBAction func titlebarPlaylistButtonPressed(_ sender: NSButton) {
         // If we are currently darkening...
         if(darkening) {
-            // If the darkening is not from the playlist button...
-            if(titlebarContainer.subviews.last != titlebarPlaylistButton) {
+            // If the playlist isn't already open...
+            if(!playlistOpen) {
                 // Darken and open the playlist
                 darken(withTitlebarItemAbove: titlebarPlaylistButton);
                 openPlaylist();
             }
-            // If the darkening is from the playlist button...
+            // If the playlist is already open...
             else {
-                // Undarken
+                // Undarken and close the playlist
                 undarken();
+                closePlaylist();
             }
         }
         // If we aren't currently darkening...
@@ -52,16 +53,17 @@ class AZMusicPlayerViewController: NSViewController {
     @IBAction func titlebarSearchButtonPressed(_ sender: NSButton) {
         // If we are currently darkening...
         if(darkening) {
-            // If the darkening is not from the search button...
-            if(titlebarContainer.subviews.last != titlebarSearchButton) {
+            // If search isn't already open...
+            if(!searchOpen) {
                 // Darken and open search
                 darken(withTitlebarItemAbove: titlebarSearchButton);
                 openSearch();
             }
-            // If the darkening is from the search button...
+            // If search is already open...
             else {
-                // Undarken
+                // Undarken and close search
                 undarken();
+                closeSearch();
             }
         }
         // If we aren't currently darkening...
@@ -163,6 +165,18 @@ class AZMusicPlayerViewController: NSViewController {
     /// The length of the darken fade animation
     let darkenAnimationDuration : CGFloat = 0.25;
     
+    /// The container view for the playlist view controller
+    @IBOutlet weak var playlistContainerView: NSView!
+    
+    /// The container view for the search view controller
+    @IBOutlet weak var searchContainerView: NSView!
+    
+    /// Is the playlist open?
+    var playlistOpen : Bool = false;
+    
+    // Is search open?
+    var searchOpen : Bool = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
@@ -170,14 +184,151 @@ class AZMusicPlayerViewController: NSViewController {
         styleWindow();
     }
     
+    /// The last timer created by 'close(Playlist/Search)' for doing post-animation stuff
+    private var lastCloseAnimationTimer : Timer? = nil;
+    
     /// Opens the playlist view
     func openPlaylist() {
-        print("Playlist");
+        // Cancel 'lastCloseAnimationTimer' in case the user is spamming the button or something
+        lastCloseAnimationTimer?.invalidate();
+        lastCloseAnimationTimer = nil;
+        
+        // Move the the playlist view to the top
+        self.view.addSubview(playlistContainerView, positioned: .above, relativeTo: nil);
+        
+        // If search is open...
+        if(searchOpen) {
+            // Show the playlist container
+            playlistContainerView.isHidden = false;
+            
+            // Hide the view and move it to the bottom of the subviews
+            searchContainerView.isHidden = true;
+            self.view.addSubview(searchContainerView, positioned: .below, relativeTo: nil);
+            
+            // Say search isn't open
+            searchOpen = false;
+        }
+        // If search isn't open...
+        else {
+            // Set the opacity of the playlist container to 0(just in case there's a split second where you can see pre-animation)
+            playlistContainerView.layer?.opacity = 0;
+            
+            // Show the playlist container
+            playlistContainerView.isHidden = false;
+            
+            // Create the slide animation
+            let playlistSlide : CABasicAnimation = CABasicAnimation(keyPath: "position.y");
+            playlistSlide.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut);
+            playlistSlide.duration = CFTimeInterval(0.5);
+            playlistSlide.fromValue = -(playlistContainerView.frame.height);
+            playlistSlide.toValue = 0;
+            
+            // Add the animation to the playlist container
+            self.playlistContainerView.layer?.add(playlistSlide, forKey: "animateContents");
+        }
+        
+        // Set the alpha value so we can actually see the view
+        self.playlistContainerView.layer?.opacity = 1;
+        
+        // Say the playlist is open
+        playlistOpen = true;
+    }
+    
+    /// Closes the playlist view
+    func closePlaylist() {
+        // Create the slide animation
+        let playlistSlide : CABasicAnimation = CABasicAnimation(keyPath: "position.y");
+        playlistSlide.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn);
+        playlistSlide.duration = CFTimeInterval(0.3);
+        playlistSlide.fromValue = 0;
+        playlistSlide.toValue = -(playlistContainerView.frame.height);
+        
+        // Add the animation to the playlist container
+        self.playlistContainerView.layer?.add(playlistSlide, forKey: "animateContents");
+        
+        // Wait until the animation is over
+        lastCloseAnimationTimer = Timer.scheduledTimer(withTimeInterval: TimeInterval(0.25), repeats: false, block: { timer in
+            // Hide the playlist container
+            self.playlistContainerView.isHidden = true;
+            
+            // Move the playlist container to the back again
+            self.view.addSubview(self.playlistContainerView, positioned: .below, relativeTo: nil);
+        });
+        
+        // Say the playlist is closed
+        playlistOpen = false;
     }
     
     /// Opens the search view
     func openSearch() {
-        print("Search");
+        // Cancel 'lastCloseAnimationTimer' in case the user is spamming the button or something
+        lastCloseAnimationTimer?.invalidate();
+        lastCloseAnimationTimer = nil;
+        
+        // Move the the search view to the top
+        self.view.addSubview(searchContainerView, positioned: .above, relativeTo: nil);
+        
+        // If the playlist is open...
+        if(playlistOpen) {
+            // Show the search container
+            searchContainerView.isHidden = false;
+            
+            // Hide the view and move it to the bottom of the subviews
+            playlistContainerView.isHidden = true;
+            self.view.addSubview(playlistContainerView, positioned: .below, relativeTo: nil);
+            
+            // Say the playlist isn't open
+            playlistOpen = false;
+        }
+        // If the playlist isn't open...
+        else {
+            // Set the opacity of the search container to 0(just in case there's a split second where you can see pre-animation)
+            searchContainerView.layer?.opacity = 0;
+            
+            // Show the search container
+            searchContainerView.isHidden = false;
+            
+            // Create the slide animation
+            let searchSlide : CABasicAnimation = CABasicAnimation(keyPath: "position.y");
+            searchSlide.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut);
+            searchSlide.duration = CFTimeInterval(0.5);
+            searchSlide.fromValue = -(searchContainerView.frame.height);
+            searchSlide.toValue = 0;
+            
+            // Add the animation to the search container
+            self.searchContainerView.layer?.add(searchSlide, forKey: "animateContents");
+        }
+        
+        // Set the alpha value so we can actually see the view
+        self.searchContainerView.layer?.opacity = 1;
+        
+        // Say search is open
+        searchOpen = true;
+    }
+    
+    // Closes the search view
+    func closeSearch() {
+        // Create the slide animation
+        let searchSlide : CABasicAnimation = CABasicAnimation(keyPath: "position.y");
+        searchSlide.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn);
+        searchSlide.duration = CFTimeInterval(0.3);
+        searchSlide.fromValue = 0;
+        searchSlide.toValue = -(searchContainerView.frame.height);
+        
+        // Add the animation to the search container
+        self.searchContainerView.layer?.add(searchSlide, forKey: "animateContents");
+        
+        // Wait until the animation is over
+        lastCloseAnimationTimer = Timer.scheduledTimer(withTimeInterval: TimeInterval(0.25), repeats: false, block: { timer in
+            // Hide the search container
+            self.searchContainerView.isHidden = true;
+            
+            // Move the search container to the back again
+            self.view.addSubview(self.searchContainerView, positioned: .below, relativeTo: nil);
+        });
+        
+        // Say search is closed
+        searchOpen = false;
     }
     
     /// Darkens the window, and if withTitlebarItemAbove is passed either 'titlebarPlaylistButton' or 'titlebarSearchButton' it will keep that view above the darken
